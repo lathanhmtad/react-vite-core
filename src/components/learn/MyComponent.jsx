20cfd4ebce1508e645da3224245f407bcd9087be
lathanhmtad <d0763705638@gmail.com>
2024-06-28 16:06:43 +0700

    variable with jsx

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,9 +3,21 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const a = 'Eric2' // string
+    // const a = 25 // number
+    // const a = true // boolean
+    // const a = undefined 
+    // const a = null
+    const a = [1,2,3]
+    // const a = {
+    //   name: "duy",
+    //   age: 25
+    // }
+
     return (
       <>
-        <div>Eric & Hoidanit update</div>
+        <div>{JSON.stringify(a)} & Hoidanit update</div>
+        <div>{console.log('ERIC')}</div>
         <div style={{borderRadius: "10px"}} className="child">Child</div>
       </>
     )
