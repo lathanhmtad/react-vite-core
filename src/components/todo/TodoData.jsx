3b03cbc9c6569d01d7af73af2abd9cf30222fd2f
lathanhmtad <d0763705638@gmail.com>
2024-06-28 20:12:50 +0700

    learn props

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,20 @@
+const TodoData = (props) => {
+    // props là một object {}
+
+    // const { name, age, data } = props
+    // const name = props.name
+    // const age = props.age
+    // const data = props.data
+
+    // console.log(props)
+
+    return (
+        <div className="todo-data">
+            <div>My name is {name}</div>
+            <div>Learning React</div>
+            <div>Watching Youtube</div>
+        </div>
+    )
+}
+
+export default TodoData
