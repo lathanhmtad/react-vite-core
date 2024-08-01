f29d6537e6a37dc4d4634f60a21a691350572c82
lathanhmtad <d0763705638@gmail.com>
2024-08-01 09:56:52 +0700

    using uncontrolled component for register page

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,55 @@
+import { Button, Input, Form } from "antd"
+
 const RegisterPage = () => {
+    const [form] = Form.useForm()
+
+    const onFinish = (values) => {
+        console.log(values)
+    }
+
     return (
-        <div>register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        >
+
+            <div style={{
+                margin: "50px",
+                // display: "flex",
+                // gap: "15px",
+                // flexDirection: "column"
+            }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone number"
+                    name="phone"
+                >
+                    <Input />
+                </Form.Item>
+                <div>
+                    <Button onClick={() => form.submit()} type="primary">Register</Button>
+                </div>
+            </div>
+        </Form>
     )
 }
 
